c606e90cb9ddc68866244889a4041cb9ad62795a
Matthew Thorning <matthew@ometria.com>
2022-08-18 16:44:55 +0100

    remove keytest route

--- src/routes/keytest/+page.server.ts
+++
@@ -1,5 +0,0 @@
-import { secretAccessKey, accessKeyId } from '$lib/aws/constants';
-
-export async function load() {
-  return { secretAccessKey, accessKeyId };
-}
--- src/routes/keytest/+page.svelte
+++
@@ -1,8 +0,0 @@
-
-<script lang="ts">
-  import type { PageServerData } from './$types';
-
-  export let data: PageServerData;
-</script>
-
-<pre>{JSON.stringify(data)}</pre>
