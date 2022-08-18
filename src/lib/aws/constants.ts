59baa871c1429a76e064ea9a51ad05bf3866706d
Matthew Thorning <matthew@ometria.com>
2022-08-18 15:39:01 +0100

    rename AWS env vars

--- src/lib/aws/constants.ts
+++ src/lib/aws/constants.ts
@@ -1,7 +1,7 @@
-import { AWS_BUCKET_NAME, AWS_CLOUDFRONT_URL, AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID, } from '$env/static/private';
+import { AWS_BUCKET_NAME, AWS_CLOUDFRONT_URL, AWS_ACCESS_SECRET, AWS_ACCESS_KEY, } from '$env/static/private';
 
 export const s3BucketPrefix = 'photos/';
 export const s3BucketName =  AWS_BUCKET_NAME;
-export const accessKeyId =  AWS_ACCESS_KEY_ID
-export const secretAccessKey = AWS_SECRET_ACCESS_KEY
+export const accessKeyId =  AWS_ACCESS_KEY
+export const secretAccessKey = AWS_ACCESS_SECRET
 export const imageHandlerUrl = AWS_CLOUDFRONT_URL
