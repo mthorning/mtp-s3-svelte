3cca204737e678c12a93deb0aa1c441f8e3967ec
Matt Thorning <mattthorning@Matts-MacBook-Pro.local>
2022-08-28 18:53:56 +0100

    remove config options

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import inject from '@rollup/plugin-inject'
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })],
-    },
-  },
   plugins: [sveltekit()],
 });
